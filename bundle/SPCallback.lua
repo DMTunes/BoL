b3f3be57aa6b5c6aab3a58f5cc4bffdb86bce3f7
spyk <spyk1@users.noreply.github.com>
2016-12-02 01:54:46 +0100

    Create SPCallback.lua

---
+++ bundle/SPCallback.lua
@@ -0,0 +1,149 @@
+LoadEncryptedScript([[0Y4Qesy/PrUo7RFocMjcPZ0uj7VDrC7Nn2rybyI7oEPVWtDlKMeBwcnQX8bVElZvpsUf6fbFFotpWlwIgCLeMUoPQGaseG3RM6ChMAFWp0bcAliTx8Z82qiBzLenWuo2iI9A3okwmqg5N0qiU5FPB+
+NKacjKf5M4rHaoYiLWAmhGIG6d4Kp0/7oauPjZsGzUQWm/ZNtB50unvjVCqboboK6ySix/j9GKpbUVW3qMTiQ526vxEpfOurpKW+lc48fHHNf6sQrKOwXQQ+si1aogvrNlKxP0oXpLwL+BcR8glyRH
+l1bxYwPqt1Lm2AJMF6YuVBg9Bi/0VhwKBVUpI8SoffM0i28It4ULyT/baRgQgIilHYDAXj1mqnCHPrglYUnKc6XliEqosWPNJJD8enQMqSCm3VccE1HwzTLQstc2GEEnQfP/SiZFutEDgOB6qRGuNh
+8EiM4L4pPVxQwi2bpTBIRDeclrxxjAGxYX2yHY4Mp+Mw5Aejr2xoDjXZTliNh8jtIuaU4nlsaZ5hCq9vmo+Z91hDiPM5tpR7K/DhcpHWCu8vRTKIW6WLYKopSSBIko2pKc1xq11CZYL15oVy6Hs1Hq
+hhyB9Udz/Z4ZWkFN8mn7PP9S94bZSGB71uKpdh0Xgo6hYPpd8VYOi7myfd8qVwHVo0XM+SOTFgFP3PtzRDxMC7FAcaTi8pZXN5DHrWCoqXND3ADNNwSZPA/Sule7pGIubgelXxaHV2DRTojsoQl3gu
+PghzLfz0x7+ITeZmgnx/wq/57XhktyIU+K8Eq+e6SRUvgyVxYsXq+rHeUL9odfQbYK4Cl5ii6DXiic6cNvkgKqlMA0UKOXyWQBNlrMf7B1es7eu0ee9JVNYyjQWmYHkXJSFh2E3/IZRmat+jbGNvwI
+50QVFPuker4bV+YuHE0woeMDxhlkvCDU1tHnjGnZ8olIMJOVhTsww12j6qw0lwvjbu9XBR/7D5vKDmYh9WLqiva+kuVMsyZXNvvsDayC2xwjZlZZiMa1YLF9UPtbjbgM6s+ZJQEaycbGKebDb4EJUi
+kJ/aBHfC0D+56cCnyeGVt9aVR800PhMR4lYhAbLv5Qjer+wNLEP5ohB7Ykbiqb7QHRH9HDo3yWl5z7XcLIUfHRcPempNMJMZCLqaXZp+usIcLOsZvWM/FIdjt24FMEUc2dKeBz6NLDzw2xDjFOLcp3
+3c8OF3xKVCOb/lfZRx4tCWHISkxK9EvZRGHwN08m/5iNc5DDIybO7vNjmm6/YGFlU4FlTc5Q+80ZIP2bHgYBJrYQTl7LfZsGK64EMnFiMWe0t70iT09XPaKWcF6x1oLRILO4S+FtlGRrIsd8BV35Rm
+74PyuSpm4kTWe4QIl3G2XR+bEGH8O5fq4PxMaaVNcp6ihlmbPa+xFpAljXtpF25lmH6Z+67Z1ytwQL9MG+k8jgexjphxFHg1F26MunZ9cE+rk9uN3ESHHvOZ9xlL8u+GdKgvHKlZ+z3QP48ao90BZU
+kuq7nz4lcj5ZrYODYE7Hu1Zb+3Qdx0XCUacZjZPYyLoRzc9dDJJOAgMzBRjxdO53pj6tJ4fMwF5DzUi26Ih24+y065Nz6bb3dNEM5kpq2hUKNwZejlHAVvun6WOmlAZTc5v2xcUsMyMn8izqIdendt
+/7ulQ4X8AEKh2HwHsSLF4D2sO6NZPsS6a9zUNnOKu1Is4owfpmKEKoVgEa/3xFFVg4SqS5jSOWvyLcyjV5L5hzsSv9UpmCrVwws1Mft+VFBvFFge2z4H+Dv/+Ba6sUEVtBbls6AORO3/Q7HFP1uQCP
+QQMFZT0qG0aT8Z/SYJDvUB1EGjA9ZHV6Hg7kmx0rc2jXOXOANDfkHyEjyiJb0tFiKQEs/t55mbUByDM6N1l2Es/R44njs3Gkxyrs4ES/UCf26RIsoXYM0STOchmzr7aAmx0WbRVXvhwZmrkx+dLIwo
+rW/WZNl1BNKxSEv8Y9pwpaMaVKMSRPBOlcIAdzCiVHZv+8SGh8wv/mwC3YfrqByqLp5wHHmernYYrtNCJvQLGypSRZzVC/aTZD+ny4G+u39yB4I6MK3GLFK6w9eoR++D0lyESDyIj9Np6cjt2XBN7C
+zkcW3SOSvcKMJajzYogZH7303jzi2LBdrQlFgSs/f6EQoJXWGTfqqDsBLmwehKjbD1Fz40L4qAOhb9zsOhdCxnh6N7jh9dqrV02PB+/Bgr8s8syWpbMMHWsX34rCUb4gMWFo1fmuBIrnizfHr3wXyq
+QDLGoNZauJUT2+s8Z3jfSYDH89UCCS5xcJRafp2f13K29wqfek1wM7QmMYWXSQ8iCV//TJxT/Pll8ozeFztgmeB5E4Yq34mkD0XXWQk3480fSi+8NTGJt3/YFWJXDuT9wfLTOhLdVKAMHhcx6JK16T
+bkg5KcapH0L6AfvFSk+Vwrh2WMXqHTKoUAVikaUXitgRxVoNOqQ4fVOWKL+0puoVZwRm0ZLOcsCYEaIgYrdPVYti8zYOclGNfY+tEA4cufijQfKKKFV7OExSeWdlbmSRheA75PQ6Ds9Y4vLmt0PGrI
+58xR99j2OysZEXqaeb4N3JEtg0Mz9obqOiRr2pG5cLs24KbgdzEu9ZpN7E+hG1kIbzmYdiaTcKeqTvUlZaTRmnFZ1mzRcVRp0sBlBQEQkYtfSgv8P58tfqJAM1egdWYV8DdrYXzCM1yAFq0kWUQFoa
+NBFn8CqREl15uCsaLDXHJ0amvG1Q/1MAees4iwzw95VcKzb1aXHKdUMci0eCnJrSezjNfQ4EkUJiy/sE+AspmglOBf5L7TMBYjKTuJ+dYf+pbpriOzPII0ASbyKMXZhfza3CQFqWstnQYA1+mMMkXz
+TweODHYOV38N+jGggyuWp4dUR5pIJV2rWWSEvm3dUb1BtW2P7JboNwtZXKop7ECRqPgHK/Zx+4cRpXIKv6oP7eLi+3/YLMWyueYLlZkQpXnpQ0OQ6NN6i8HOt5zS3yhxcGnOkJH+PltidM0DN2YAqg
+dfnr6/jiBDRuWQ33E870MQt3m1oAM2x3ssKpeB6BrZBnkMzWYyd9Vb7e20brwJxYkkhmXIPdf7VojBYqdlGnFF03D1yZIeJcWQWl35oyMOu5IInGD/aVCxnGhhp7zGZM9pD709MBjSEklHHRI+4pv2
+KCIOFQMRrcWAx3WxfuCt8jTfHOFbKEombTEzykdU0oz3cOa3DIDz6pIpStshy+FdewDVAS2i5Y7KOhIgc1TKaosoyYK4ISCEHG1wmAYsSViVjo4CDxRsu8+UdMeRS8RZSEb+3qSxLX1Zl235RQEO0e
+ATULhgH8gOJLGYyfsVIqKchxiDMvs//WLrIiJw58FW2iS2kE5Am3s75el8BYqKbM8IkLG4yDHQs7083qrpcNXUxW+hp1lyUVBt6aE5McvGqPVmSpi6Ul43sr8FPiwTYudX/JsMWRAsU/OlcR/OoYyQ
+tWguoQKW07Ckl/fyyxFa4vfFAFGwsaUkgtNSIleB0CuIEOyUg]],_ENV.FILE_NAME,"e577dc2863e3fbb3952fe6abe9bd5702")
+
+function SPCallback:Var()
+	self.OnDashSpell = {
+		{name = "syndraq"},
+		{name = "ahritumble", duration = .25}, -- Ahri's r
+		{name = "akalishadowdance", duration = .25}, -- Akali r
+		{name = "headbutt", duration = .25}, -- Alistar w
+		{name = "caitlynentrapment", duration = .25}, -- Caitlyn e
+		{name = "carpetbomb", duration = .25}, -- Corki w
+		{name = "dianateleport", duration = .25}, -- Diana r
+		{name = "fizzpiercingstrike", duration = .25}, -- Fizz q
+		{name = "fizzjump", duration = .25}, -- Fizz e
+		{name = "gragasbodyslam", duration = .25}, -- Gragas e
+		{name = "gravesmove", duration = .25}, -- Graves e
+		{name = "ireliagatotsu", duration = .25}, -- Irelia q
+		{name = "jarvanivdragonstrike", duration = .25}, -- Jarvan q
+		{name = "jaxleapstrike", duration = .25}, -- Jax q
+		{name = "khazixe", duration = .25}, -- Khazix e
+		{name = "leblancslide", duration = .25}, -- Leblanc w
+		{name = "leblancslidem", duration = .25}, -- Leblanc w (r)
+		{name = "blindmonkqtwo", duration = .25}, -- Lee sin q
+		{name = "blindmonkwone", duration = .25}, -- Lee sin w
+		{name = "luciane", duration = .25}, -- Lucian e
+		{name = "maokaiunstablegrowth", duration = .25}, -- Maokai w
+		{name = "nocturneparanoia2", duration = .25}, -- Nocturne r
+		{name = "pantheon_leapbash", duration = .25}, -- Pantheon e
+		{name = "renektonsliceanddice", duration = .25}, -- Renekton e
+		{name = "riventricleave", duration = .25}, -- Riven q
+		{name = "rivenfeint", duration = .25}, -- Riven e
+		{name = "sejuaniarcticassault", duration = .25}, -- Sejuani q
+		{name = "shenshadowdash", duration = .25}, -- Shen e
+		{name = "shyvanatransformcast", duration = .25}, -- Shyvana r
+		{name = "rocketjump", duration = .25}, -- Tristana w
+		{name = "slashcast", duration = .25}, -- Tryndamere e
+		{name = "vaynetumble", duration = .25}, -- Vayne q
+		{name = "viq", duration = .25}, -- Vi q
+		{name = "monkeykingnimbus", duration = .25}, -- Wukong q
+		{name = "xenzhaosweep", duration = .25}, -- Xin xhao q
+		{name = "yasuodashwrapper", duration = .25}, -- Yasuo e
+	};
+	self.ImmobileSpell = {
+		{name = "katarinar", duration = 1}, -- Katarina R
+		{name = "drain", duration = 1}, -- Fiddle W
+		{name = "crowstorm", duration = 1}, --Fiddle R
+		{name = "consume", duration = .5}, -- Nunu Q
+		{name = "absolutezero", duration = 1}, -- Nunu R
+		{name = "rocketgrab", duration = .5}, -- Blitzcrank Q
+		{name = "staticfield", duration = .5}, -- Blitzcrank R
+		{name = "cassiopeiapetrifyinggaze", duration = .5}, -- Cassio's R
+		{name = "ezrealtrueshotbarrage", duration = 1}, -- Ezreal's R
+		{name = "galioidolofdurand", duration = 1}, -- Ezreal's R
+		{name = "luxmalicecannon", duration = 1}, -- Lux R
+		{name = "reapthewhirlwind", duration = 1}, -- Jannas R
+		{name = "jinxw", duration = .6}, -- JinxW
+		{name = "jinxr", duration = .6}, -- JinxR
+		{name = "missfortunebullettime", duration = 1}, -- MissFortuneR
+		{name = "shenstandunited", duration = 1}, -- ShenR
+		{name = "threshe", duration = .4}, -- ThreshE
+		{name = "threshrpenta", duration = .75}, -- ThreshR
+		{name = "infiniteduress", duration = 1}, -- Warwick R
+		{name = "meditate", duration = 1}, -- Yi W
+		{name = "summonerteleport", duration = 4.5}, -- Teleportation
+	};
+	self.InterruptableSpell = {
+		{name = "katarinar", duration = 1}, -- Katarina R
+		{name = "galioidolofdurand", duration = 1}, -- Galio R
+		{name = "crowstorm", duration = 1}, -- Fiddle R
+		{name = "drain", duration = 1}, -- Fiddle W
+		{name = "absolutezero", duration = 1}, -- Nunu R
+		{name = "shenstandunited", duration = 1}, -- Shen R
+		{name = "urgotswap2", duration = 1}, -- Urgot R
+		{name = "alzaharnethergrasp", duration = 2.5}, -- Malzahar R
+		{name = "fallenone", duration = 1.5}, -- Karthus R
+		{name = "pantheon_grandskyfall_jump", duration = 1.8}, -- Pantheon R
+		{name = "varusq", duration = 1}, -- Varus Q
+		{name = "caitlynaceintthehole", duration = 1}, -- Caitlyn R
+		{name = "missfortunebullettime", duration = 2.5}, -- MissFortune R
+		{name = "infiniteduress", duration = 2}, -- Warwick R
+		{name = "lucianr", duration = 2}, -- Lucian R
+		{name = "jhinr", duration = 2.5}, -- Jhin R
+	};
+	self.AntiGapCloserSpell = {
+		{name = "akalidhadowdance"};
+		{name = "headbutt"};
+		{name = "dianateleport"};
+		{name = "ireliagatotsu"};
+		{name = "jaxleapstrike"};
+		{name = "jaycetotheskies"};
+		{name = "maokaiunstablegrowth"};
+		{name = "monkeykingnimbus"};
+		{name = "pantheon_leapbash"};
+		{name = "poppyheroiccharge"};
+		{name = "quinne"};
+		{name = "xenzhaosweep"};
+		{name = "blindmonkqtwo"};
+		{name = "fizzpiercingstrike"};
+		{name = "rengarleap"};
+		{name = "yasuodashwrapper"};
+		{name = "aatroxq"};
+		{name = "gragase"};
+		{name = "gravesmove"};
+		{name = "hecarimult"};
+		{name = "jarvanivdragonstrike"};
+		{name = "jarvanivcataclysm"};
+		{name = "khazixe"};
+		{name = "khazixelong"};
+		{name = "leblancslidem"};
+		{name = "leblancslide"};
+		{name = "leonazenithblade"};
+		{name = "ufslash"};
+		{name = "renektonsliceanddice"};
+		{name = "sejuaniarcticassault"};
+		{name = "shenshadowdash"};
+		{name = "rocketjump"};
+		{name = "slashcast"};
+	};
+end
+
+-- For Debug
+
+-- AddProcessSpellCallback(function(unit, spell)
+-- 	if unit and unit.valid and unit.isMe then
+-- 		print(spell.name:lower());
+-- 	end
+-- end);
