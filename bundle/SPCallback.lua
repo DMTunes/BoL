2bfea27ca55855dc0659020556a2956b80abdafb
spyk <spyk1@users.noreply.github.com>
2017-02-26 03:16:09 +0100

    Update SPCallback.lua

--- bundle/SPCallback.lua
+++ bundle/SPCallback.lua
@@ -1,32 +1,94 @@
-LoadEncryptedScript([[DJUu3t+xj6rSnnh0nEYJQJrQ1oSgj/cKOEw9k2lxkn6Mn3CAcSed56dL89LVU1AGjaxo8WEt7tUGBs8RITMPDKlqSYtt5tnvdKP8n5grLJfeeYYn+em26jjq6kOul1UY5J5mwus/gyt66G0JACH+Dt
-zMI3oqLcz9QFyjZV+uvm1x4V9OyuCj47V6Uitr2ClZLWVeCoQbribred/mGqNQVZ48/QTgLj0lGIAXknHFo8PMm+O5sruOKHjd/7ncJY9dC9pXtZCMMFWypVWSJ6/GGYjjbGyrsm1v/gjLHM+dSdf8
-K5rZ208b3wI/+S7q04TX3JgSBAZ9W+F/y3fYsX0fy025o39ctvMh9PDSiHgd0C5d0JfRsf8PY3AWzPzOJSzERl+5J1VpzOzyTu32TvKS4q3kpcQI3pWqoZJ/LrNlx+MCrS2q0ZSBnQFhSZ7GX0CHdB
-WQ88n+UF2KX8MW7EtfSO0nFz41K5IgCmxbQ24E8eCEOQLdgodayTSVkUoh5o0N3xx6YES9YmKkQFioa92zPKLeuy65y9383pP6jt5sV2/BenVAMinvroxd8BSSn4G6iywa5GhyJsi6UvZvwkPQC/WL
-rEUMcxKTwfv6hji7DsurleNyxIXoMWDOAAc/nTgz4y+RIRi5lskn5YsCVf6RlNuD2CasDKA7nLfe/UgmsyrLSMerj8oCwKeJh0MdIvtcJ0CAycHcZPW2+mbic6srUlDsz1jQkF+hWnScx120tmrwDY
-PYMOtusLfQtO53JteoEAtFptSVdX6jIAEZAc7FQ2jDYsVcm3bXkn0NLSA7kPQ+aYv8bHCRO0mRtqoWADN9g/lNKFcLWjzyOA5Dv466iv8VwS1eZX0SMtLZ2rz8TOF1+9+tJc7DCahHG3FydLiBTD6p
-7NxB4QwyXN1ZIgkFM/yES4uvxvZPYxGeKnCaEjBXwbh3VRmMofflb6lamDGFZDDcUg1uliFPnAXTl6Mhf/1hH2EX73yVhRaMLvrc6MLE/dNV1ke2PETifAHA5gMvTI74OWc+zA3lV3wtW/9DnRKqsj
-G3g2HIDEPcXq4u89sJjgDj6yduKgp4EzD9FG/kxCAoyfHzh1NZTuQeKimVMXKBmmCF1d62uCh8X2Ieqf9C/uiswHN/z2zm8klYKJ/38NcwhZYPJNpXQN/6kVKWqk9QBuIULPzkJhBf86Bmg3kOpdNf
-OycUtuOECDs77HeoLAcOG87/BTSIM9i5Jf57R8IphHrQfpjxD567yrcO2t/eh5sLk1hkMwUMmbSLhnFZFQwZNvNA9iNhbYMU7IInWNwbLL39qpddtfjvzrKf0OtKwT0G7ZGZECJKOnoChSwdxEAwbn
-nw+9jr8AaMf2b1S+Iu0mVFSaoi39+tT5+MOlSaaNkfsgeDxdP/VNVs4P1P/joqmr12ZIS0Pij89XPuqfY99zlwD1u0yQzcwpK9CqYkg01wlreBz34533YHFT/AsN9AySqgkBYujQnntPRJwT0sXkAZ
-1GItklzdy4Yd4Kh5MZq9MPEyNj9GYJqHG34+Nq3DwZ9VmFLXWuryPBWhOcyTr8hhIA/56q3EJbSH75UqFNcW+rwqlDt3a8SUbvhiuu9nOkWV9mUW92MpzxbqSlsnhgYIyyUP+8U/ccEAzYEYI34luj
-PcBQacAsAUc2FZRy7zBZQdLeVTiKSiVY1C15unGIJZneNPOU0DSDMZ6FwBawi/SLJhXCtYfPpOGtFCk56GGNzU6O3EqcPKyuN7zUqjRLbQK4DoDrVZl3/x85aKki+T9V9hJwdfAr/K78uITrFqie7F
-Zs1fvzY31JY+lEpW6RyODOocxvSzgL53V3BohkKNIYUQBN1R29Tpq57nQMEx8IHhEHWfmHgsW9VFtZDffywX4F30DGQIkAI/6pK98HOPUxs93F4vU93wCHr8cx72QhRFD0ebxvwKpbJLsgRDYGIWe3
-yY74Wl3p2iYtZGUVUJgWQv0UkH2+ORQgUGo5N32/H9mFSKTXbggjsWfU2OJe/mIbPwoMn3drhWaPnnkYoZHbRgOOCFLm8/ozLomqZbD4RfRQn7daBZmZqAyXNrIwN5McsF6EigczDQ6HrVGieF0EpK
-51Bk76t/nRV/U02jsCppPFVhku3eYzKjFv53/vN5E9abXr3El7KhKSNTEGWFL9adt24gf9Kp00rfdfro8iJtaLD/rEwv7iWtALCOTu156KPT2A7bFlDLPTiJ+O7376FhEcWE7E8DI6ypDKqnEKSwGq
-QTKlETyZ1BR/i4ZUhjHkvqpGAyTTlC9YO1bRLf9151aogaxju7Z5dxfGGH+G5z7RwYCHqCavfLfxU5miu+oLNyBnse7o25i/TF7H17undtoWKVjqVc1kls8f+bpPnsLhE3hr9RV5VePDjr0BHaWoZL
-mi2VaQuTHK4hOqmO5O7AhGMjwfwY1OGLQCFRXFdSkmWLF54mrSp3u+vUeztifL0h9K7ZFaM9XVq9ezVQzlhONcPBsjUjxBTugmV5iCcB9Gqdnxd5loDy/QPBfPpKDTr2GDO+wnUGdCyy6n5skYOhDB
-LNZaENATQFc8wc40RXA5BzNtgHiJWMx19QvphC3PjK2QhmRNu4q/KStfVe7b3jA6uebOJIDo9OSYs18rPKTFpfoodwBzT+9eHK6pOqRVvhGM43eTeBVspKU+ZujwNI5zJY2Xlmu4IqeELGZTQzrByq
-ZPvmonZ/uOwmvVZtah5/LTPn083nsjZ8+P+BwQeY9K9GMEx1/ZfO/4TA7R7Pgt8SU6jOD0EhFZSxA/uxf/aSjumgnF0hhbSHG9e/Wk09pzlygyvScoLf4StIPp4+71aLmAHK+RTKIWHlamx9stfED8
-nB1j5QVK4nlg22aWABuHK/K/ZKvsyUU2iz2t3Odfz6pdgyDHfAxVgaPDU9c4CsWT5v9AqehSukjt8FDt3LnZ55Vl8zAgLKIlWEhUNOeGD9qNC6thZaUhrjcu8Jpmo7EQ/UAUKKk7VVDkqfjjKD8ql3
-eem17bId8t/6783vKobzuhZhJ5a7e5CJhmjMQdaLV3LKua+LXR5PAN37CZWZtD+NuLShWljFLkHGHIAdS+3I5eC6pp7vdoSbxWlDyKvACOBKQgFwaGYszOBcjX2g7MiZnUZUgy83/LjR2QMDsrBqAs
-vfYzrku/mY2hLJRROCb7ENP/d1uCDgpIugr7LN9ettMW0WdSfCiyrhAS9dfrzeqU2FktVuv5EG7nGZdgRvj145z6BMtolLZAVkB6yMwndQXLVzlvZO/tArp4e7XOHzZQ9Ncsl6MWpmG71RbpQ9nI6S
-jygC7S8sLQ5jKt1rT4tFSpvn3/Pn2U93ckTZ6vqCdOBo1WFWMBC+I/AebKG1zhLhNSHv6a1wREbtYtBIkk+yRw7xFnh9bb85QU8R0XGW556cQS+d45tTP41nMFa95NjiAfccHo7U7gDeKMCPq0KM3b
-ZygPdaOB3TQ31AFW/c4uA83SPw6q8jfno3y8pS5uvyRilvGR/3KFQwhkcnXcVVa0By5bw+b6PB+0n2TEy9MqN/Jp2GSrrtvbz2RBnhLmOqLlNU5N9Px55ESXWwzcTNj4MDyx+BfJVEH19hydtvTXNp
-slHXVxuw4qJtutb+v24fWnuighlaTPCf9YutRQRraFMmx5ClP1LkRaBQaoHfxJ6jrZqbKpiMYglQzyvn1DLTPqfVqWi85rNfjgUKInqfBfgu2W2DQZTNsyRUBKWcKS1Izqsti9SL20NlnLRmMHgtRj
-5NYzeJlmgWezD2MZRBMrPMoX7HMKIi4nZQkQvt7jEYIHP352cUyvV/7rBafp0Br9yEKPlwmHoLU05Yv6lYSAlCWJo+OawAfaVgauvCtGaySwEyZohSp9XjFsSdxx3tOimGocmE5NTLgCNbWyPVrNwu
-MtOI17Qdavq/5HWvLAZa5qV/FKLudhOcefPddBjhwfFjksd3ug14DElYGWsIuQx4ySeCs2VMcIdlRg2lLFF69Q24KI38m5rsmHAEVoKRltM1xBD3c2qjscIVznF75Y4UI6fW3tXbgQku+9N/Lux2LD
-dAFbcMDGM2GukKzw8ZI2MV6LqQfL5d93eQxs3OKUYcvjTwFP5Ao6Zopow8XHDGIg+ZLsYtxRpEVo60oFi2EX9YpGh6BIbKy5SFlFXiK3w9wGZwJfg3Hn71qFKBQ/J1xO+4K/D8kM5P+f5ebRUHN8Jb
-J28mGeQU3NmFOJp7ZDf0k2XlJEerFHRixDT/jrniI+9s1l/xWckgNKaakmnibmf/uyHFJHd6J0HRjXL0kShLBJLbgC1js8oaAEn923v890ZGZv681VSmqvqSBOFDlk3OknU]],_ENV.FILE_NAME,"372ae91d0766e5bebf94ca2e11aefb54")
+class "SPCallback";
+
+function SPCallback:__init()
+	self:Var();
+	AddProcessSpellCallback(function(unit, spell)
+		self:OnProcessSpell(unit, spell);
+	end);
+	AddApplyBuffCallback(function(source, unit, buff)
+		self:ApplyBuff(source, unit, buff);
+	end);
+	AddNewPathCallback(function(unit, startPos, endPos, isDash ,dashSpeed,dashGravity, dashDistance) 
+		self:OnNewPath(unit, startPos, endPos, isDash, dashSpeed, dashGravity, dashDistance) 
+	end);
+	OnDashCast, InterruptableCast, AntiGapCloserCast, ImmobileCast = nil;
+end
+
+function SPCallback:OnNewPath(unit, startPos, endPos, isDash, dashSpeed ,dashGravity, dashDistance)
+	if isDash and OnDashCast and unit.team ~= myHero.team then
+		local dash = {};
+		if unit.type == myHero.type then
+			dash.unit = unit;
+			dash.startPos = startPos;
+			dash.endPos = endPos;
+			dash.speed = dashSpeed;
+			dash.startT = os.clock() - GetLatency() / 2000;
+			dash.endT = dash.startT + (GetDistance(startPos, endPos) / dashSpeed);
+		end
+		OnDashCast(dash)
+	end
+end
+
+function SPCallback:ApplyBuff(unit, t, buff)
+	if unit and unit.valid and unit.type == myHero.type and unit.team ~= myHero.team and ImmobileCast and GetDistance(unit) < 1500 then
+		if buff.type == 5 or buff.type == 11 or buff.type == 24 or buff.type == 29 then
+			ImmobileCast(unit, buff);
+		end
+		if self.ImmobileBuff[buff.name:lower()] then
+			ImmobileCast(unit, buff);
+		end
+	end
+end
+
+function SPCallback:OnProcessSpell(unit, spell)
+	if unit and unit.valid and unit.type == myHero.type and unit.team ~= myHero.team and spell and spell.name and GetDistance(unit) < 1500 then
+		if InterruptableCast then
+			for k, v in ipairs(self.InterruptableSpell) do
+				if spell.name:lower() == self.InterruptableSpell[k].name then
+					InterruptableCast(unit, spell);
+				end
+			end
+		end
+		if ImmobileCast then
+			for k, v in ipairs(self.ImmobileSpell) do 
+				if spell.name:lower() == self.ImmobileSpell[k].name then
+					ImmobileCast(unit, spell);
+				end
+			end
+		end
+		if AntiGapCloserCast then
+			for k, v in ipairs(self.AntiGapCloserSpell) do
+				if spell.name:lower() == self.AntiGapCloserSpell[k].name then
+					AntiGapCloserCast(unit, spell);
+				end
+			end
+		end
+	end
+end
+
+function SPCallback:OnDash(func)
+	assert(func and type(func) == "function" and OnDashCast == nil, "Invalid OnDashCallback");
+	OnDashCast = func;
+end
+
+function SPCallback:Interruptable(func)
+	assert(func and type(func) == "function" and InterruptableCast == nil, "Invalid InterruptableCallback");
+	InterruptableCast = func;
+end
+
+function SPCallback:AntiGapCloser(func)
+	assert(func and type(func) == "function" and AntiGapCloserCast == nil, "Invalid AntiGapCloserCallback");
+	AntiGapCloserCast = func;
+end
+
+function SPCallback:Immobile(func)
+	assert(func and type(func) == "function" and AntiGapCloserCast == nil, "Invalid ImmobileCallback");
+	ImmobileCast = func;
+end
+
+function SPCallback:v()
+	return "26022017"
+end
 
 function SPCallback:Var()
 	self.OnDashSpell = {
